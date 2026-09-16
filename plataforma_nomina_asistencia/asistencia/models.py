22efca28c31b6bd3b6edbf7295bc97749f0ef74e
Sebasval1122 <svalencia170@soyudemedellin.edu.co>
2026-09-16 17:38:53 -0500

    Corrección de django

--- plataforma_nomina_asistencia/asistencia/models.py
+++ plataforma_nomina_asistencia/asistencia/models.py
@@ -1,1 +1,10 @@
-# Modelo de marcajes de asistencia
+from usuarios.models import Usuario
+from sucursales.models import Sucursal
+from django.db import models
+
+class Asistencia:
+    if Usuario is not Usuario.sucursal:
+        print("Registre al usuario primero o sucursal equivocada")
+    else:
+
+        pass
